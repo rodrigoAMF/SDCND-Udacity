c800d6ea1edbe93f1e41d32e7b56927a772b6ed0
Rodrigo Franco <rodrigoamfranco@gmail.com>
2021-07-27 23:07:26 -0300

    P4 Training using efficientNetB0

--- P4-Behavioral-Cloning/drive.py
+++ P4-Behavioral-Cloning/drive.py
@@ -60,7 +60,8 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
+        #image_array = np.asarray(image)
+        image_array = np.asarray(image)[64:140, 0:320, :]
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
